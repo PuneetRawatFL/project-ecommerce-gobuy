af0da232bfc1f0cd21e08e34d6058023f0dfc564
Puneett2507 <puneet.rawat2507@gmail.com>
2024-12-17 23:00:42 +0530

    header and footer included from another html

--- homeScript.js
+++ homeScript.js
@@ -1,15 +1,24 @@
+//fetch header navbar
+document.addEventListener("DOMContentLoaded", () => {
+	fetch("header.html")
+		.then((response) => response.text())
+		.then((data) => {
+			document.querySelector(".navbar").innerHTML = data;
+		});
+});
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
+	acc[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
 }
 
 // carousel scroll
@@ -21,18 +30,18 @@ const nextBtn = document.querySelector(".next-btn");
 let index = 0;
 //carous
 function showNext() {
-    if (index < carouselItems.length - 4) {
-        index++;
-        console.log(carouselItems.length);
-        carousel.style.transform = `translateX(-${index * 25}%)`; // Adjust percentage based on items shown
-    }
+	if (index < carouselItems.length - 4) {
+		index++;
+		console.log(carouselItems.length);
+		carousel.style.transform = `translateX(-${index * 25}%)`; // Adjust percentage based on items shown
+	}
 }
 
 function showPrev() {
-    if (index > 0) {
-        index--;
-        carousel.style.transform = `translateX(-${index * 25}%)`; // Adjust percentage based on items shown
-    }
+	if (index > 0) {
+		index--;
+		carousel.style.transform = `translateX(-${index * 25}%)`; // Adjust percentage based on items shown
+	}
 }
 
 nextBtn.addEventListener("click", showNext);
@@ -46,21 +55,30 @@ const productCardPrice = document.querySelector("#product-card-price");
 var productCard = document.querySelectorAll(".product-card");
 
 (function displayProductCard() {
-    fetch("https://fakestoreapi.com/products")
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
+	fetch("https://fakestoreapi.com/products")
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
 
-            productCard.forEach((product) => {
-                var rand = Math.floor(Math.random() * 10);
-                // console.log(rand);
-                console.log(product.children[1].innerText);
-                console.log(data[rand]);
-                //to display image
-                product.children[0].src = data[rand].image;
-                //to display price
-                // product.children[1].innerText = `$${data[rand].price}`;
-                // product.children[0].src = data[rand].image;
-            });
-        });
+			productCard.forEach((product) => {
+				var rand = Math.floor(Math.random() * 10);
+				// console.log(rand);
+				console.log(product.children[1].innerText);
+				console.log(data[rand]);
+				//to display image
+				product.children[0].src = data[rand].image;
+				//to display price
+				// product.children[1].innerText = `$${data[rand].price}`;
+				// product.children[0].src = data[rand].image;
+			});
+		});
 })();
+
+//fetch footer
+document.addEventListener("DOMContentLoaded", () => {
+	fetch("footer.html")
+		.then((response) => response.text())
+		.then((data) => {
+			document.querySelector("footer").innerHTML = data;
+		});
+});
--- script.js
+++ script.js
@@ -1,101 +1,110 @@
+//fetch header navbar
+document.addEventListener("DOMContentLoaded", () => {
+	fetch("header.html")
+		.then((response) => response.text())
+		.then((data) => {
+			document.querySelector(".navbar").innerHTML = data;
+		});
+});
+
 // access accordian
 var acc = document.getElementsByClassName("accordion");
 
 for (var i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
+	acc[i].addEventListener("click", function () {
+		this.classList.toggle("active");
 
-        //access content
-        var panel = this.nextElementSibling;
+		//access content
+		var panel = this.nextElementSibling;
 
-        //show and hide content
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
+		//show and hide content
+		if (panel.style.display === "block") {
+			panel.style.display = "none";
+		} else {
+			panel.style.display = "block";
+		}
 
-        //add animation in panel opening
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
+		//add animation in panel opening
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
 }
 
 //function to add loading bar
 function loadingBar() {
-    var loadingContainer = document.querySelector(".loading-container");
-    console.log("loading container");
-    loadingContainer.style.visibility = "visible";
+	var loadingContainer = document.querySelector(".loading-container");
+	console.log("loading container");
+	loadingContainer.style.visibility = "visible";
 }
 //function to remove loading bar
 function removeLoadingBar() {
-    var loadingContainer = document.querySelector(".loading-container");
-    loadingContainer.style.visibility = "hidden";
+	var loadingContainer = document.querySelector(".loading-container");
+	loadingContainer.style.visibility = "hidden";
 }
 
 //function to manipulate dom and add items
 function getProducts(url) {
-    //enable loading slider
-    loadingBar();
-    fetch(url)
-        .then((res) => res.json())
-        .then((products) => {
-            // remove loading slider
-            removeLoadingBar();
+	//enable loading slider
+	loadingBar();
+	fetch(url)
+		.then((res) => res.json())
+		.then((products) => {
+			// remove loading slider
+			removeLoadingBar();
 
-            //accessing containers
-            const prodContainer = document.querySelector(".items-list");
+			//accessing containers
+			const prodContainer = document.querySelector(".items-list");
 
-            //clear previously fetched items
-            prodContainer.innerHTML = " ";
+			//clear previously fetched items
+			prodContainer.innerHTML = " ";
 
-            //creating loop for each product
-            products.forEach((product) => {
-                // console.log(product);
+			//creating loop for each product
+			products.forEach((product) => {
+				// console.log(product);
 
-                // creating a div for each product
-                const productDiv = document.createElement("div");
-                productDiv.classList.add("product-item");
+				// creating a div for each product
+				const productDiv = document.createElement("div");
+				productDiv.classList.add("product-item");
 
-                // adding title
-                const title = document.createElement("p");
-                title.classList.add("product-name");
-                title.textContent = product.title;
+				// adding title
+				const title = document.createElement("p");
+				title.classList.add("product-name");
+				title.textContent = product.title;
 
-                //creating image div
-                const imgDiv = document.createElement("div");
-                imgDiv.classList.add("product.image");
+				//creating image div
+				const imgDiv = document.createElement("div");
+				imgDiv.classList.add("product.image");
 
-                //adding image
-                const image = document.createElement("img");
-                image.classList.add("prod-image");
-                image.src = product.image;
-                image.alt = product.title;
-                imgDiv.appendChild(image);
+				//adding image
+				const image = document.createElement("img");
+				image.classList.add("prod-image");
+				image.src = product.image;
+				image.alt = product.title;
+				imgDiv.appendChild(image);
 
-                //adding price
-                const price = document.createElement("p");
-                price.classList.add("product-price");
-                price.textContent = `Price: $${product.price}`;
+				//adding price
+				const price = document.createElement("p");
+				price.classList.add("product-price");
+				price.textContent = `Price: $${product.price}`;
 
-                //adding 'add to cart' button
-                const btn = document.createElement("button");
-                btn.classList.add("addButton");
-                btn.textContent = "Add to Cart";
+				//adding 'add to cart' button
+				const btn = document.createElement("button");
+				btn.classList.add("addButton");
+				btn.textContent = "Add to Cart";
 
-                // Append elements to productDiv
-                productDiv.appendChild(imgDiv);
-                productDiv.appendChild(title);
-                productDiv.appendChild(price);
-                productDiv.appendChild(btn);
+				// Append elements to productDiv
+				productDiv.appendChild(imgDiv);
+				productDiv.appendChild(title);
+				productDiv.appendChild(price);
+				productDiv.appendChild(btn);
 
-                // Append the product div to the container
-                prodContainer.appendChild(productDiv);
-            });
-        });
+				// Append the product div to the container
+				prodContainer.appendChild(productDiv);
+			});
+		});
 }
 //invoke function by default
 getProducts("https://fakestoreapi.com/products");
@@ -112,24 +121,24 @@ let cat5 = document.querySelector("#cat5"); //womens clothing
 
 //all clothing category
 cat1.addEventListener("click", () => {
-    getProducts("https://fakestoreapi.com/products");
-    console.log("category 4");
+	getProducts("https://fakestoreapi.com/products");
+	console.log("category 4");
 });
 //electronics category
 cat2.addEventListener("click", () => {
-    getProducts("https://fakestoreapi.com/products/category/electronics");
+	getProducts("https://fakestoreapi.com/products/category/electronics");
 });
 //jewelry category
 cat3.addEventListener("click", () => {
-    getProducts("https://fakestoreapi.com/products/category/jewelery");
+	getProducts("https://fakestoreapi.com/products/category/jewelery");
 });
 //men clothing category
 cat4.addEventListener("click", () => {
-    getProducts("https://fakestoreapi.com/products/category/men's clothing");
+	getProducts("https://fakestoreapi.com/products/category/men's clothing");
 });
 //women clothing category
 cat5.addEventListener("click", () => {
-    getProducts("https://fakestoreapi.com/products/category/women's clothing");
+	getProducts("https://fakestoreapi.com/products/category/women's clothing");
 });
 
 //sort by filter
@@ -137,17 +146,26 @@ let sort1 = document.querySelector("#sort1");
 let sort2 = document.querySelector("#sort2");
 //ascending
 sort1.addEventListener("click", () => {
-    getProducts("https://fakestoreapi.com/products?sort=asc");
-    console.log("category 4");
+	getProducts("https://fakestoreapi.com/products?sort=asc");
+	console.log("category 4");
 });
 //descending
 sort2.addEventListener("click", () => {
-    getProducts("https://fakestoreapi.com/products?sort=desc");
-    console.log("category 4");
+	getProducts("https://fakestoreapi.com/products?sort=desc");
+	console.log("category 4");
 });
 
 //reset button
 let resetBtn = document.querySelector(".reset");
 resetBtn.addEventListener("click", () => {
-    getProducts("https://fakestoreapi.com/products");
+	getProducts("https://fakestoreapi.com/products");
+});
+
+//fetch footer
+document.addEventListener("DOMContentLoaded", () => {
+	fetch("footer.html")
+		.then((response) => response.text())
+		.then((data) => {
+			document.querySelector("footer").innerHTML = data;
+		});
 });
