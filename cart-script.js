bb0adf9aea5a4855d98fd55a057e8170b6611ddb
Puneett2507 <puneet.rawat2507@gmail.com>
2025-01-12 20:33:49 +0530

    cart total in progress

--- cart-script.js
+++ cart-script.js
@@ -1,150 +1,169 @@
-// let itemImg = document.querySelector("#item-image"); //item image
-// console.log(itemImg.src); - done
-// let itemName = document.querySelector("#item-details"); //item name
-// console.log(itemName.innerText); - done
-
 // cart total elemnt
 let cartTotal = document.querySelector("#cart-total-price-span");
 
 function addItemInCart(arr) {
-    console.log(arr[arr.length - 1]);
-    console.log(arr[arr.length - 1].id);
+	// let cartItemDiv = document.querySelector(".cart-items");
+	let cartItemDiv = document.createElement("div");
+	cartItemDiv.classList.add("cart-items");
+	cartItemDiv.id = `cart-${arr[arr.length - 1].id}`;
 
-    // let cartItemDiv = document.querySelector(".cart-items");
-    let cartItemDiv = document.createElement("div");
-    cartItemDiv.classList.add("cart-items");
-    cartItemDiv.id = `${arr[arr.length - 1].id}-cart`;
-    console.log("Cart id", cartItemDiv.id);
+	//creating img div
+	const itemImgDiv = document.createElement("div");
+	itemImgDiv.classList.add("item-image");
+	itemImgDiv.classList.add("item");
 
-    //creating img div
-    const itemImgDiv = document.createElement("div");
-    itemImgDiv.classList.add("item-image");
-    itemImgDiv.classList.add("item");
+	// creating img tag
+	const itemImg = document.createElement("img");
+	itemImg.id = "item-image";
+	itemImg.src = arr[arr.length - 1].image;
 
-    // creating img tag
-    const itemImg = document.createElement("img");
-    itemImg.id = "item-image";
-    itemImg.src = arr[arr.length - 1].image;
+	//appending img tag to div
+	itemImgDiv.append(itemImg);
 
-    //appending img tag to div
-    itemImgDiv.append(itemImg);
+	//creating stack items div
+	const stackItems = document.createElement("div");
+	stackItems.classList.add("stack-items");
 
-    //creating stack items div
-    const stackItems = document.createElement("div");
-    stackItems.classList.add("stack-items");
+	// div for item detail and price
+	const itemDetails = document.createElement("div");
+	itemDetails.classList.add("itemDetails");
+	itemDetails.classList.add("item");
 
-    // div for item detail and price
-    const itemDetails = document.createElement("div");
-    itemDetails.classList.add("itemDetails");
-    itemDetails.classList.add("item");
+	//p tags for detail and price
+	const cartItemName = document.createElement("p");
+	cartItemName.id = "item-details";
+	cartItemName.textContent = arr[arr.length - 1].title;
 
-    //p tags for detail and price
-    const cartItemName = document.createElement("p");
-    cartItemName.id = "item-details";
-    cartItemName.textContent = arr[arr.length - 1].title;
+	const cartItemPrice = document.createElement("p");
+	cartItemPrice.id = "item-price";
+	cartItemPrice.textContent = `$${arr[arr.length - 1].price}`;
 
-    const cartItemPrice = document.createElement("p");
-    cartItemPrice.id = "item-price";
-    cartItemPrice.textContent = arr[arr.length - 1].price;
+	// appending p tags to item detail div
+	itemDetails.append(cartItemName);
+	itemDetails.append(cartItemPrice);
 
-    // appending p tags to item detail div
-    itemDetails.append(cartItemName);
-    itemDetails.append(cartItemPrice);
+	//creating quantity counter div
+	const itemQuantityDiv = document.createElement("div");
+	itemQuantityDiv.classList.add("item-quantity");
+	itemQuantityDiv.classList.add("item");
 
-    //creating quantity counter div
-    const itemQuantityDiv = document.createElement("div");
-    itemQuantityDiv.classList.add("item-quantity");
-    itemQuantityDiv.classList.add("item");
+	//creating quantity counter container
+	const itemQuantityContainer = document.createElement("div");
+	itemQuantityContainer.classList.add("item-quantity-container");
 
-    //creating quantity counter container
-    const itemQuantityContainer = document.createElement("div");
-    itemQuantityContainer.classList.add("item-quantity-container");
+	//creating buttons and div for item count
+	//increase btn
+	const itemIncrease = document.createElement("button");
+	itemIncrease.classList.add("counter");
+	itemIncrease.id = "itemIncrease";
+	itemIncrease.innerText = "+";
 
-    //creating buttons and div for item count
-    //increase btn
-    const itemIncrease = document.createElement("button");
-    itemIncrease.classList.add("counter");
-    itemIncrease.id = "itemIncrease";
-    itemIncrease.innerText = "+";
+	//count div
+	const itemCount = document.createElement("div");
+	itemCount.id = "itemCount";
+	//p tag for count
+	const pItemCount = document.createElement("p");
+	pItemCount.innerText = arr[arr.length - 1].quantity;
+	pItemCount.id = "itemQuantity";
+	//appending
+	itemCount.append(pItemCount);
 
-    //count div
-    const itemCount = document.createElement("div");
-    itemCount.id = "itemCount";
-    //p tag for count
-    const pItemCount = document.createElement("p");
-    pItemCount.innerText = "1";
-    //appending
-    itemCount.append(pItemCount);
+	//decrease btn
+	const itemDecrease = document.createElement("button");
+	itemDecrease.classList.add("counter");
+	itemDecrease.id = "itemDecrease";
+	itemDecrease.innerText = "-";
 
-    //decrease btn
-    const itemDecrease = document.createElement("button");
-    itemDecrease.classList.add("counter");
-    itemDecrease.id = "itemDecrease";
-    itemDecrease.innerText = "-";
+	//appending buttons and counter to parent container
+	itemQuantityContainer.append(itemIncrease);
+	itemQuantityContainer.append(itemCount);
+	itemQuantityContainer.append(itemDecrease);
 
-    //appending buttons and counter to parent container
-    itemQuantityContainer.append(itemIncrease);
-    itemQuantityContainer.append(itemCount);
-    itemQuantityContainer.append(itemDecrease);
+	// appending
+	itemQuantityDiv.append(itemQuantityContainer);
 
-    // appending
-    itemQuantityDiv.append(itemQuantityContainer);
+	//appending to item stack items div
+	stackItems.append(itemDetails);
+	stackItems.append(itemQuantityDiv);
 
-    //appending to item stack items div
-    stackItems.append(itemDetails);
-    stackItems.append(itemQuantityDiv);
+	//appending to main div
+	cartItemDiv.append(itemImgDiv);
+	cartItemDiv.append(stackItems);
 
-    //appending to main div
-    cartItemDiv.append(itemImgDiv);
-    cartItemDiv.append(stackItems);
+	// append to list
+	document.querySelector(".cart-items-list").append(cartItemDiv);
 
-    // append to list
-    document.querySelector(".cart-items-list").append(cartItemDiv);
+	//change cart total
+	let totalPrice = `${(
+		parseFloat(cartTotal.innerText) +
+		parseFloat(arr[arr.length - 1].price * arr[arr.length - 1].quantity)
+	).toFixed(2)}`;
+	cartTotal.innerText = totalPrice;
+	// modifyCartTotal(arr, "add");
 
-    //change cart total
-    cartTotal.innerText = `${(
-        parseFloat(cartTotal.innerText) + parseFloat(arr[arr.length - 1].price)
-    ).toFixed(2)}`;
+	//increase counter
+	itemIncrease.addEventListener("click", () => {
+		//increasing quantity
+		arr[arr.length - 1].quantity++;
+		pItemCount.innerText = arr[arr.length - 1].quantity;
 
-    //increase decrease counter
-    itemIncrease.addEventListener("click", () => {
-        let counterCount = pItemCount.innerText;
-        counterCount++;
-        pItemCount.innerText = counterCount;
-        console.log("item increase");
-    });
+		//to increase cart item number
+		window.updateCartItem("increase");
 
-    itemDecrease.addEventListener("click", () => {
-        let counterCount = pItemCount.innerText;
-        counterCount--;
-        console.log(cartItemDiv.id);
-        if (counterCount == 0) {
-            let index;
-            console.log(cartItemDiv.id[0]);
-            for (let x = 0; x < window.globalCartArray.length; x++) {
-                if (cartItemDiv.id[0] == window.globalCartArray[x].id) {
-                    index = arr.indexOf(window.globalCartArray[x]);
-                    console.log("item found to remove", index);
+		//update cart total
+		window.modifyCartTotal();
+	});
 
-                    // --------------------------------
-                    // to remove the item from the cart list
-                    // - from list
-                    let cartList = document.querySelector(".cart-items-list");
-                    let removeItem = cartList.children[index];
-                    console.log(removeItem);
-                    cartList.removeChild(removeItem);
-                    // - from array ----------------------------- pending
-                }
-            }
-            console.log("index", index);
-        }
-        pItemCount.innerText = counterCount;
-        console.log("item decrease");
-    });
+	//decrease counter
+	itemDecrease.addEventListener("click", () => {
+		//decreasing quantity
+		arr[arr.length - 1].quantity--;
+		pItemCount.innerText = arr[arr.length - 1].quantity;
+
+		//to decrease cart item number
+		window.updateCartItem("decrease");
+
+		//remove item from list and array
+		if (arr[arr.length - 1].quantity == 0) {
+			let index;
+			for (let x = 0; x < window.globalCartArray.length; x++) {
+				if (cartItemDiv.id[5] == window.globalCartArray[x].id) {
+					index = arr.indexOf(window.globalCartArray[x]);
+					// --------------------------------
+					// to remove the item from the cart list
+					// - from list
+					let cartList = document.querySelector(".cart-items-list");
+					let removeItem = cartList.children[index];
+					cartList.removeChild(removeItem);
+
+					// - from array
+					window.globalCartArray.splice(index, 1); // this will remove the item from global array
+				}
+			}
+		}
+
+		//update cart total
+		window.modifyCartTotal();
+	});
 }
 
 //making this function global
 window.addItemInCart = addItemInCart;
+
+//function to modify total price
+function modifyCartTotal() {
+	let totalPrice = 0;
+	for (let index = 0; index < window.globalCartArray.length; index++) {
+		totalPrice +=
+			window.globalCartArray[index].price *
+			window.globalCartArray[index].quantity;
+	}
+	totalPrice = parseFloat(totalPrice.toFixed(2));
+
+	cartTotal.innerText = totalPrice;
+}
+
+window.modifyCartTotal = modifyCartTotal;
 
 // window.hello = hello;
 // window.addItemInCart();
--- header-script.js
+++ header-script.js
@@ -1,39 +1,49 @@
 //loading cart-modal
 let visible = "false";
 $(function () {
-  $(".cart-modal").load("cart.html", function () {
-    $.getScript("cart-script.js");
-    //to close the cart
-    document.querySelector(".cart-close").addEventListener("click", () => {
-      if (visible === "true") {
-        document.querySelector(".cart-modal").style.right = "-100%";
-        visible = "false";
-        // document.querySelector(".blur").style.filter = "blur(0px)";
-      }
-    });
-  });
-  document.querySelector(".shopping-cart").addEventListener("click", () => {
-    if (visible === "false") {
-      document.querySelector(".cart-modal").style.right = "0";
-      visible = "true";
-      // document.querySelector(".blur").style.filter = "blur(5px)";
-      // console.log(window.globalCartArray);
-    }
-  });
+	$(".cart-modal").load("cart.html", function () {
+		$.getScript("cart-script.js");
+		//to close the cart
+		document.querySelector(".cart-close").addEventListener("click", () => {
+			if (visible === "true") {
+				document.querySelector(".cart-modal").style.right = "-100%";
+				visible = "false";
+				// document.querySelector(".blur").style.filter = "blur(0px)";
+			}
+		});
+	});
+	document.querySelector(".shopping-cart").addEventListener("click", () => {
+		if (visible === "false") {
+			document.querySelector(".cart-modal").style.right = "0";
+			visible = "true";
+			// document.querySelector(".blur").style.filter = "blur(5px)";
+			// console.log(window.globalCartArray);
+		}
+	});
 });
 
 //local storage
-localStorage.setItem("cartCountStorage", 0);
+// localStorage.setItem("cartCountStorage", 0);
 
-let cartCountStorage = 1;
+let cartCountStorage = 0;
 //function to increase cart count
-function updateCartItem() {
-  const cartCount = document.querySelector(".cart-item-count");
-  if (cartCount) {
-    cartCount.innerText = cartCountStorage;
-    cartCountStorage++;
-  }
-  cartCount.style.visibility = "visible";
+function updateCartItem(str) {
+	const cartCount = document.querySelector(".cart-item-count");
+	if (str === "increase") {
+		cartCountStorage += 1;
+		if (cartCountStorage) {
+			cartCount.innerText = cartCountStorage;
+		}
+		cartCount.style.visibility = "visible";
+	} else if (str === "decrease") {
+		cartCountStorage -= 1;
+		if (cartCountStorage) {
+			cartCount.innerText = cartCountStorage;
+		} else {
+			cartCount.style.visibility = "hidden";
+		}
+	}
 }
+
 //to make function available globally
 window.updateCartItem = updateCartItem;
