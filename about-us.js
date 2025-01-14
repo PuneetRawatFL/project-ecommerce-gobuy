02177fd276d1c08ff3de17885e0d08e279bd4739
Puneet Rawat <Puneet.Rawat@xoriant.com>
2025-01-14 17:04:40 +0530

    about us page customization

--- about-us.js
+++ about-us.js
@@ -1,12 +1,37 @@
 // fetching header, footer and announcement bar
 //loading header script
 $(function () {
-  //announcement bar
-  $(".announcement-bar").load("announcement-bar.html");
-  //header
-  $(".navbar").load("header.html", function () {
-    $.getScript("header-script.js");
-  });
-  //footer
-  $("#footer").load("footer.html");
+    //announcement bar
+    $(".announcement-bar").load("announcement-bar.html");
+    //header
+    $(".navbar").load("header.html", function () {
+        $.getScript("header-script.js");
+    });
+    //footer
+    $("#footer").load("footer.html");
 });
+
+// carousel scroll
+const carousel = document.querySelector(".carousel");
+const carouselItems = document.querySelectorAll(".carousel-item");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+let index = 0;
+//carousal click
+function showNext() {
+    if (index < carouselItems.length - 4) {
+        index++;
+        carousel.style.transform = `translateX(-${index * 25}%)`; // Adjust percentage based on items shown
+    }
+}
+
+function showPrev() {
+    if (index > 0) {
+        index--;
+        carousel.style.transform = `translateX(-${index * 25}%)`; // Adjust percentage based on items shown
+    }
+}
+
+nextBtn.addEventListener("click", showNext);
+prevBtn.addEventListener("click", showPrev);
